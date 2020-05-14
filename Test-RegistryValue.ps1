a8309eaf341b316615f6e1b4f813839c320e4a68
James E <jespinoza84@gmail.com>
2020-05-14 09:18:08 -0400

    Add files via upload

---
+++ Test-RegistryValue.ps1
@@ -0,0 +1,28 @@
+﻿function Test-RegistryValue
+{
+  [cmdletbinding()]
+  param(
+    [parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]$Path,
+    
+    [parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]$Value
+  )
+
+  try {
+    
+    Get-ItemProperty -Path $Path | Select-Object -ExpandProperty $Value -ErrorAction Stop | Out-Null
+    return $True
+    
+     
+  } catch {
+  
+    return $False
+    
+  
+  }
+  
+}
+
+# Example 
+# Test-RegistryValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Value "Lync"
