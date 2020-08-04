cd9fca6228c08c4c9584431e8e976be8576eeb7a
James E <jespinoza84@gmail.com>
2020-08-04 10:25:05 -0400

    Add files via upload

---
+++ MECM/Find-CMClientByGUID.ps1
@@ -0,0 +1,18 @@
+﻿# For problematic clients in SMS_MP_Control_Manager
+
+function Find-CMClientByGUID {
+    [cmdletbinding()]
+    param(
+    [Parameter(Mandatory=$true)]
+    [string]$GUID,
+    [Parameter(Mandatory=$true)]
+    [string]$PrimaryServer
+    )
+    
+    $params = @{Namespace=”ROOT\SMS\site_NOR“
+    Class=”SMS_R_System”
+    ComputerName=$PrimaryServer
+    Recurse = $true}
+
+    Get-WmiObject @params | Where-Object {$_.SMSUniqueIdentifier -match $GUID.TrimStart("GUID:")}
+}
