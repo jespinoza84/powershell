ae9fb79e4f404ec0e9127683c12be461d23500ba
James E <jespinoza84@gmail.com>
2020-10-31 17:06:47 -0400

    Update Get-SpeedTestResults.ps1

--- WIP/Get-SpeedTestResults.ps1
+++ WIP/Get-SpeedTestResults.ps1
@@ -1,6 +1,16 @@
-﻿# speedtest.exe --help
-# for more info
+<# 
 
-# Look to converting to scheduled task w/ burnt toast notification
+Download SpeedTest CLI @ https://www.speedtest.net/apps/cli
+.\speedetest.exe --help
 
-$Speedtest = .\speedtest.exe -A --format csv --output-header | ConvertFrom-Csv
+WIP:
+- Look to converting to scheduled task w/ burnt toast notification
+
+
+#>
+$url = 'https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-win64.zip'
+Invoke-RestMethod -Uri $url -OutFile .\download.zip
+Expand-Archive -Path .\download.zip | Out-Host
+
+
+# $Speedtest = .\speedtest.exe -A --format csv --output-header | ConvertFrom-Csv
